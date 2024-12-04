41f804ad04bd3235fb1c86ca67d2616d6fc64aeb
cashewwww14 <151065728+cashewwww14@users.noreply.github.com>
2024-12-04 07:42:14 +0700

    Create FirstClassTicket.java

---
+++ Polymorphism/FirstClassTicket.java
@@ -0,0 +1,10 @@
+public class FirstClassTicket extends Ticket {
+    public FirstClassTicket(String passengerName, double baseFare) {
+        super(passengerName, baseFare);
+    }
+
+    @Override
+    public double calculateFare() {
+        return getBaseFare() * 1.5; // 50% additional charge
+    }
+}
